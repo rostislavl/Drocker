bfd209a5f18050348e3fd4baa4b9ded86cea896a
Rastsislau Liaukovich, CN Group <leerimich@gmail.com>
2015-12-04 13:33:20 +0100

    Separate configs for version Drupal 7 and Drupal 8

---
+++ 7.x/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ ! -f /var/www/$DRUPAL_BASE_DIR/www/index.php ]; then
+
+    # Install Drupal codebase
+    drush make -y /var/www/drush.make /var/www/$DRUPAL_BASE_DIR/www
+    chown -R www-data:www-data /var/www/$DRUPAL_BASE_DIR/www
+    echo "=========================================================================="
+    echo "Drupal codebase successfully installed in /var/www/$DRUPAL_BASE_DIR/www"
+    echo "=========================================================================="
+
+    # Install Drupal site
+    #cd /var/www/$DRUPAL_BASE_DIR/www
+    #drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://YourMySQLUser:RandomPassword@localhost/YourMySQLDatabase
+
+fi
+
+/usr/bin/supervisord
