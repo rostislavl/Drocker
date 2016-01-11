e9abcd63adfa74f4092a1d82361bd31403aedce9
Rastsislau Liaukovich, CN Group <liaukovich@cngroup.dk>
2016-01-11 03:00:38 +0100

    Remove comments from entrypoint file

--- containers/drupalapp1/entrypoint.sh
+++ containers/drupalapp1/entrypoint.sh
@@ -5,10 +5,10 @@ if [ ! -f /var/www/$DRUPAL_BASE_DIR/www/index.php ]; then
 
     # Install Drupal codebase
     drush make -y /var/www/drush.make /var/www/$DRUPAL_BASE_DIR/www
+    #echo "=========================================================================="
+    #echo "Drupal codebase successfully installed in /var/www/$DRUPAL_BASE_DIR/www"
+    #echo "=========================================================================="
     chown -R www-data:www-data /var/www/$DRUPAL_BASE_DIR/www
-    echo "=========================================================================="
-    echo "Drupal codebase successfully installed in /var/www/$DRUPAL_BASE_DIR/www"
-    echo "=========================================================================="
 
     # Install Drupal site
     #cd /var/www/$DRUPAL_BASE_DIR/www
